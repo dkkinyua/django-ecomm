0f58c964a50f1a25ce03b0b4c9a6ff0ebb4e3b26
unknown <denzelkinyua11@gmail.com>
2024-06-11 14:45:35 +0300

    Commit: Created all models.

--- core/models.py
+++ core/models.py
@@ -36,7 +36,7 @@ class Product(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='vendor')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
     slug = models.SlugField(unique=True)
-    price = models.FloatField()
+    price = models.FloatField() # If there is an error regarding prices, change this to DecimalField
     stock = models.PositiveIntegerField(default=1)
     name = models.CharField(max_length=75)
     description = models.TextField()
@@ -65,11 +65,110 @@ class OrderItem(models.Model):
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-    session_id = models.CharField()
-    item = models.ManyToManyField(Product, through='CartItem')
+    session_id = models.CharField(max_length=50, null=True, blank=True)
+    items = models.ManyToManyField(Product, through='CartItem')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now= True)
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     quantity = models.PositiveIntegerField(default=1)
+
+class Shipping(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    rating = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+class Payment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order')
+    method = models.CharField(max_length=50)
+    amount = models.FloatField()
+    transaction_id = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.method
+    
+class Coupon(models.Model): # For discounts in shop.
+    code = models.CharField(max_length=50, unique=True)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+
+    def __str__(self):
+        return self.code
+    
+class Review(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customer')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
+    
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    products = models.ManyToManyField(Product, related_name='products')
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sent_at
+    
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    slug = models.SlugField(unique=True)
+    title = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now= True)
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=75)
+    answer = models.TextField()
+
+    def __str__(self):
+        return self.question
+    
+class Contact(models.Model):
+    pass
+
+class Analytics(models.Model):
+    sales = models.DecimalField(max_digits=10, decimal_places=2)
+    traffic = models.PositiveIntegerField()
+    popular_products = models.ManyToManyField(Product, related_name='popular-products')
+    created_at = models.DateTimeField()
+
+# Site Configuration model
+class Configuration(models.Model):
+    site_name = models.CharField(max_length=30)
+    site_description = models.CharField(max_length==75)
+    site_logo = models.ImageField(upload_to='logos')
+
+class Subscription(models.Model):
+    email = models.EmailField(unique=True)
+    sub_at = models.DateTimeField(auto_now_add=True)
+    sub_to = models.DateTimeField()
+
+class Refund(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    reason = models.CharField(max_length=75)
+    amount = models.FloatField()
+    status = models.TextField()
+    requested_at = models.DateTimeField(auto_now_add=True)
+    processed_at = models.DateTimeField(null=True, blank=True)
+    is_refunded = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.reason
 
