4225f16578e3ba1f58f1f85a5209a4c2a08ae9c3
unknown <denzelkinyua11@gmail.com>
2024-06-11 13:44:57 +0300

    Commit: Added Order, OrderItem, Cart and CartItem models

--- core/models.py
+++ core/models.py
@@ -46,3 +46,30 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    products = models.ManyToManyField(Product, through='OrderItem')
+    total_price = models.FloatField()
+    shipping_address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return self.total_price
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
+    quantity = models.PositiveIntegerField(default=1)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    session_id = models.CharField()
+    item = models.ManyToManyField(Product, through='CartItem')
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    quantity = models.PositiveIntegerField(default=1)
+
