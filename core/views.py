15bf6a598dd203cf399296b11917e4379af6e538
unknown <denzelkinyua11@gmail.com>
2024-06-12 19:03:36 +0300

    Commit: Defining viewsets in views.py.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import (UserSerializer, VendorSerializer, OrderSerializer, OrderItemSerializer, CartSerializer, CartItemSerializer, ProductSerializer, PaymentSerializer, ShippingSerializer, BlogSerializer, CouponSerializer, ConfigurationSerializer, ContactSerializer, AnalyticsSerializer, FAQSerializer, TaxSerializer, SubscriptionSerializer, NotificationSerializer, ReviewSerializer, RefundSerializer, WishlistSerializer)
+
+from .models import (User, Vendor, Product, Order, Category, Cart, CartItem, OrderItem, Payment, Analytics, FAQ, Shipping, Coupon, Refund, Review, Blog, Configuration, Contact, Wishlist, Subscription, Notification, Tax)
 
 # Create your views here.
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
